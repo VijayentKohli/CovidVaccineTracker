7848c3bed0ee1b3600c528732416c47c1d79d77d
iamvkohli <vijkohli@microsoft.com>
2021-05-16 13:23:50 +0530

    Adding styles

--- src/components/VaccineTracker.tsx
+++ src/components/VaccineTracker.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 export const VaccineTracker: React.FC = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Vaccine Tracker</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  description: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
